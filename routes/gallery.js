6d0194509c6d130495917bc59af0f100db15ff24
Alexis <alexisko@hawaii.edu>
2018-02-21 16:56:21 -1000

    Created index and gallery routes

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-/*  */
-router.route('/')
+/* See a form to edit a gallery photo identified by the :id param */
+router.route('/:id/edit')
   .get((req, res) => {
+
+  });
+
+/* See a single gallery photo */
+router.route('/:id')
+  // To see a single gallery photo
+  .get((req, res) => {
+
+  })
+  // Update a single gallery photo
+  .put((req, res) => {
+
+  })
+  // Delete a single gallery photo
+  .delete((req, res) => {
+
+  });
+
+/* See a "new photo" form */
+router.route('/new')
+  .get((req, res) => {
+
+  });
+
+/* Create a new gallery photo */
+router.route('/')
+  .post((req, res) => {
 
   });
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/*  */
+/* View a list of gallery photos */
 router.route('/')
   .get((req, res) => {
 
